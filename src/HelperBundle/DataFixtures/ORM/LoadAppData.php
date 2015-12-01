ca29802725f45193c9b0a578e4975eb137d0ecd0
Paulo Victor Gomes <paulo.lima@tricae.com.br>
2015-12-01 09:35:09 -0200

    datafixtures for configuration and products

--- src/HelperBundle/DataFixtures/ORM/LoadAppData.php
+++ src/HelperBundle/DataFixtures/ORM/LoadAppData.php
@@ -2,6 +2,9 @@
 
 namespace HelperBundle\DataFixtures\ORM;
 
+use AppBundle\Infrastructure\Product\Category;
+use AppBundle\Infrastructure\Product\ExternalProduct;
+use AppBundle\Infrastructure\Product\Product;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,6 +13,7 @@ use AppBundle\Infrastructure\Core\Seller;
 use AppBundle\Infrastructure\Core\User;
 use AppBundle\Infrastructure\Core\UserRole;
 use AppBundle\Infrastructure\Order\ItemStatus;
+use AppBundle\Infrastructure\Core\Configuration;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -37,24 +41,140 @@ class LoadAppData extends AbstractFixture implements OrderedFixtureInterface, Co
         ['canceled', 'Cancelado', 'Pedido cancelado (Market ou Seller)'],
     ];
 
-    private $sellers = ['Walmart', 'aliexpress', 'biso'];
-
     private $markets = [
-        'Aliexpress' => [
+        'Kanui' => [
             'username' => 'fulano',
             'name' => 'Fulano de Tal',
             'password' => '123',
+            'configuration' =>  [
+                'kanui.base_url' => 'http://www.kanui.com.br',
+                'kanui.repository_type' => 'redis',
+                'kanui.webservice' => [
+                    'api_key' => '123123',
+                    'id_seller' => '77',
+                    'base_url' => 'http://mockserver',
+                    'uri_create_order' => '/kanui/order',
+                    'uri_confirm_order' => '/kanui/order/confirm',
+                    'uri_cancel_order' => '/kanui/order/cancel'
+                ]
+            ],
+            'product' => [
+                    'sku' => 'SKT317983',
+                    'name' => 'SKATE ROXY VAGUE PRETO',
+                    'weight' => '0.22',
+                    'height' => '21',
+                    'width' => '19',
+                    'length' => '4',
+                    'description' => 'Skate Roxy Vague preto, fabricado em madeira, com lixa emborracha',
+                    'activated_at' => '2014-05-23 16:48:40',
+                    'updated_at' => '2014-09-30 14:07:47',
+                    'category' => 'game',
+                    'brand' => 'sony',
+                    'max_price' => '500.00',
+                    'price' => '399,90',
+                    'special_price' => '350,90',
+                    'quantity' => 25,
+                    'free_shipping_rule' => NULL,
+                    'supplier_id' => '100',
+                    'sprite' => 'sprite.jpg',
+                    'link' => 'skate.html',
+            ],
+            'category' => [
+                'categoryId' => '857',
+                'categoryName' => 'Street',
+                'categoryKeyName' => 'street',
+            ]
         ],
-        'Biso' => [
+        'Tricae' => [
             'username' => 'beltrano',
             'name' => 'Beltrano da Silva',
             'password' => '123',
+            'configuration' =>  [
+                'tricae.base_url' => 'http://www.tricae.com.br',
+                'tricae.repository_type' => 'redis',
+                'tricae.webservice' => [
+                    'api_key' => '123123',
+                    'id_seller' => '78',
+                    'base_url' => 'http://mockserver',
+                    'uri_create_order' => '/tricae/order',
+                    'uri_confirm_order' => '/tricae/order/confirm',
+                    'uri_cancel_order' => '/tricae/order/cancel'
+                ]
+            ],
+            'product' => [
+                'sku' => 'MTKBX2015',
+                'name' => 'Mortal Kombat X',
+                'weight' => '0.22',
+                'height' => '21',
+                'width' => '19',
+                'length' => '4',
+                'description' => 'Mortal Kombat',
+                'activated_at' => '2014-05-23 16:48:40',
+                'updated_at' => '2014-09-30 14:07:47',
+                'brand' => 'sony',
+                'max_price' => '99.99',
+                'price' => '99.99',
+                'special_price' => '85.99',
+                'quantity' => 6,
+                'sprite' => 'mortalx.jpg',
+                'link' => 'mortalx.html',
+            ],
+            'category' => [
+                'categoryId' => '12',
+                'categoryName' => 'Game',
+                'categoryKeyName' => 'game',
+            ]
         ],
         'Dafiti' => [
             'username' => 'sicrano',
             'name' => 'Sicrano Pereira',
             'password' => '123',
-        ]
+            'configuration' =>  [
+                'dafiti.base_url' => 'http://www.dafiti.com.br',
+                'dafiti.repository_type' => 'redis',
+                'dafiti.webservice' => [
+                    'api_key' => '123123',
+                    'id_seller' => '79',
+                    'base_url' => 'http://mockserver',
+                    'uri_create_order' => '/dafiti/order',
+                    'uri_confirm_order' => '/dafiti/order/confirm',
+                    'uri_cancel_order' => '/dafiti/order/cancel'
+                ]
+            ],
+            'product' => [
+                'sku' => 'SPTLUIZA245245',
+                'name' => 'Sapato Luiza Barcelos',
+                'weight' => '0.22',
+                'height' => '21',
+                'width' => '19',
+                'length' => '4',
+                'description' => 'SAPATO',
+                'activated_at' => '2014-05-23 16:48:40',
+                'updated_at' => '2014-09-30 14:07:47',
+                'category' => 'game',
+                'brand' => 'sony',
+                'max_price' => '500.99',
+                'price' => '485.99',
+                'special_price' => '420.99',
+                'quantity' => 6,
+                'free_shipping_rule' => NULL,
+                'supplier_id' => '100',
+                'sprite' => 'sapatolb.jpg',
+                'link' => 'sapatolb.html',
+            ],
+            'category' => [
+                'categoryId' => '18',
+                'categoryName' => 'Sapato',
+                'categoryKeyName' => 'Sapato',
+            ]
+        ],
+    ];
+
+    private $sellers = [
+        'Walmart',
+        'Americanas',
+        'Extra',
+        'Shoptime'
     ];
 
     /**
@@ -79,12 +199,15 @@ class LoadAppData extends AbstractFixture implements OrderedFixtureInterface, Co
             $manager->persist($status);
         }
 
+        $sellersEntities = [];
         foreach ($this->sellers as $sellerName) {
             $seller = new Seller();
             $seller->setName($sellerName);
             $seller->setKeyName(strtolower($sellerName));
             $manager->persist($seller);
             $this->addReference($sellerName, $seller);
+            $sellersEntities[] = $seller;
+            unset($seller);
         }
 
         $role = new UserRole();
@@ -115,6 +238,51 @@ class LoadAppData extends AbstractFixture implements OrderedFixtureInterface, Co
             $user->addUserRole($role);
 
             $manager->persist($user);
+
+            // create configuration
+            foreach ($userData['configuration'] as $key => $value) {
+                $configuration = new Configuration();
+                $configuration->setMarket($market);
+                $configuration->setKey($key);
+                if (is_array($value)) {
+                    $value = json_encode($value);
+                }
+                $configuration->setValue($value);
+                $manager->persist($configuration);
+            }
+
+            // create market category
+            $category = new Category();
+            $category->setMarket($market);
+            $category->setCategoryMarketId($userData['category']['categoryId']);
+            $category->setName($userData['category']['categoryName']);
+            $category->setNameKey($userData['category']['categoryKeyName']);
+            $manager->persist($category);
+
+            // create market product
+            $product = new Product();
+            $product->setCategory($category);
+            $product->setName($userData['product']['name']);
+            $product->setSku($userData['product']['sku']);
+            $product->setPrice($userData['product']['price']);
+            $product->setStock($userData['product']['quantity']);
+            $product->setSpecialPrice($userData['product']['special_price']);
+            $product->setProductAttributes(json_encode($userData['product']));
+            $manager->persist($product);
+
+            // create one external product for each seller
+            foreach ($sellersEntities as $seller) {
+                $externalProduct = new ExternalProduct();
+                $externalProduct->setSku(uniqid());
+                $externalProduct->setJson("{}");
+                $externalProduct->setProduct($product);
+                $externalProduct->setSeller($seller);
+                $externalProduct->setStatus(ExternalProduct::STATUS_ACTIVE);
+                $manager->persist($externalProduct);
+            }
+
+
+
         }
 
         $manager->flush();
@@ -124,4 +292,5 @@ class LoadAppData extends AbstractFixture implements OrderedFixtureInterface, Co
     {
         return 1;
     }
+
 }
