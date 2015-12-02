6743700f91ff461f6d2fe911f4bd57941b62b6a9
Paulo Victor Gomes <paulo.lima@tricae.com.br>
2015-12-02 18:02:38 -0200

    init listeners

---
+++ src/AppBundle/Application/Order/QueueListener.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Application\Order;
+
+use AppBundle\Application\ApplicationEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\Event;
+use AppBundle\Application\QueueAbstract;
+
+/**
+ * Class QueueListener
+ *
+ * @author Mario Rezende
+ */
+class QueueListener extends QueueAbstract
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $serviceContainer;
+
+    public function __construct(ContainerInterface $serviceContainer)
+    {
+        $this->serviceContainer = $serviceContainer;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws QueueException
+     */
+    protected function publish($producerKey, Event $event, array $message = [])
+    {
+        $command = $event->getCommand();
+        $order = $command->orderEntity();
+        $baseMessage = $event->getMessage();
+
+        if (is_null($baseMessage)) {
+            $baseMessage = [
+                'orderId' => $order->getId(),
+                'ventureKey' => $order->getMarket()->getKeyName(),
+                'partnerKey' => $order->getSeller()->getKeyName(),
+                'timestamp' => date('c'),
+            ];
+        }
+
+        $finalMessage = array_merge($baseMessage, $message);
+
+        try {
+            $producerName = $this->handleProducerName($order->getMarket()->getKeyName(), $producerKey);
+            $this->serviceContainer->get($producerName)->publish(json_encode($finalMessage));
+
+        } catch (\Exception $exception) {
+            $exceptionMessage = [
+                'queueName' => $producerName,
+                'queueMessage' => $finalMessage,
+                'exceptionCode' => $exception->getCode(),
+                'exceptionMessage' => $exception->getMessage(),
+                'exceptionTrace' => $exception->getTraceAsString(),
+            ];
+
+            throw new \Exception(json_encode($exceptionMessage), 500);
+        }
+    }
+
+    /**
+     * @param ApplicationEvent $event
+     */
+    public function onSellerCreateOrder(ApplicationEvent $event)
+    {
+        $this->publish('seller_create_order', $event);
+    }
+
+}
---
+++ src/AppBundle/Application/QueueAbstract.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Application;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class AbstractQueue
+ */
+abstract class QueueAbstract
+{
+    /**
+     * @param string $producerKey
+     * @param Event $event
+     * @param array $message
+     */
+    abstract protected function publish($producerKey, Event $event, array $message = []);
+
+    /**
+     * @param string $ventureKey
+     * @param string $producerReference
+     * @return string
+     */
+    protected function handleProducerName($ventureKey, $producerReference)
+    {
+        $producerName = 'old_sound_rabbit_mq.'.$ventureKey.'_'.$producerReference.'_producer';
+
+        return $producerName;
+    }
+
+    /**
+     * @param array $message
+     * @return array
+     */
+    protected function handleRetryCounter(array $message)
+    {
+        $retryCounter = 0;
+
+        if (isset($message['retry'])) {
+            $retryCounter = $message['retry'] + 1;
+        }
+
+        $message['retry'] = $retryCounter;
+
+        return $message;
+    }
+
+}
